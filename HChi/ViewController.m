24bc1a33458b5b95c672fcc31c88209802eec176
是我 <uniQue@weilai.local>
2016-01-11 09:13:41 +0800

    Initial Commit

---
+++ HChi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HChi
+//
+//  Created by uniQue on 16/1/11.
+//  Copyright © 2016年 uniQue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
