fab7b0a33bb5da801e5d9bff9c91da0e89fb8a99
ZhongWeidi <git>
2016-01-11 18:47:25 +0800

    你好

---
+++ HChi/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HChi
+//
+//  Created by uniQue on 16/1/11.
+//  Copyright © 2016年 uniQue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    //你好
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
